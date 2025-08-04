356628efbb2c83c54f85ef7aa83ffc766c7f02be
AlexGett <alex_gettinger@yahoo.com>
2025-08-04 15:51:00 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'kochbuch-cache-v1';
+const urlsToCache = [
+	'./',
+	'index.html',
+	'manifest.json',
+	'favicon.ico', // Hier angepasst
+	'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js'
+];
+
+self.addEventListener('install', event => {
+	event.waitUntil(
+		caches.open(CACHE_NAME)
+		.then(cache => {
+			return cache.addAll(urlsToCache);
+		})
+	);
+});
+
+self.addEventListener('fetch', event => {
+	event.respondWith(
+		caches.match(event.request)
+		.then(response => {
+			if (response) {
+				return response;
+			}
+			return fetch(event.request);
+		})
+	);
+});
